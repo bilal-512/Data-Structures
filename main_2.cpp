2189df2dba4c1e2636732eff805755509aa112e9
Bilal <danianbilal512@gmail.com>
2026-02-19 22:15:42 +0500

    commented the code

--- main_2.cpp
+++ main_2.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    // the first argument is always interger and it holds the number of arguments
+    // the second argument is array of string which holds the string of each argument
     int sum = 0;
     for (int i = 1; i < argc; i++)
     {
